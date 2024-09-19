ec6ec2eef9bac416df35dbdeaf2ac88499d9aa53
Sush1sui <jplovesraw@gmail.com>
2024-09-19 21:39:03 +0800

    backend setup (far from done)

---
+++ backend/src/models/User.ts
@@ -0,0 +1,41 @@
+import mongoose, { mongo } from "mongoose";
+import validator from "validator";
+import bcrypt from "bcrypt";
+const { isEmail } = validator;
+
+const userSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        unique: true,
+        required: [true, "Email is required"],
+        validate: [isEmail, "Please enter a valid email"],
+    },
+    password: {
+        type: String,
+        required: [true, "Password is required"],
+        minLength: [12, "Password should be at least 12 characters"],
+    },
+});
+
+userSchema.pre("save", async function (next) {
+    this.password = await bcrypt.hash(this.password, 17);
+    next();
+});
+
+userSchema.statics.login = async function (email, password): Promise<void> {
+    if (!email) throw new Error("Email is required");
+    if (!password) throw new Error("Password is required");
+
+    const user = await this.findOne({ email });
+
+    if (user) {
+        const match = await bcrypt.compare(password, user.password);
+        if (!match) return user;
+        throw new Error("Incorrect credentials");
+    }
+    throw new Error("Incorrect credentials");
+};
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
---
+++ backend/src/server.ts
@@ -0,0 +1,19 @@
+import "dotenv/config";
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+
+const app = express();
+const PORT = process.env.PORT || 6969;
+
+app.use(cors());
+app.use(express.json());
+
+mongoose
+    .connect("mongodb://127.0.0.1:27017/genshin-wish-simulator")
+    .then(() => console.log("Connected to DB successfully"))
+    .catch((e) => console.log("Failed to connect to DB", e));
+
+app.use;
+
+app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
